baecffaa69ebf6902c2337c877f6bea6c291d072
Edgar Blau <edgar.blau@usach.cl>
2019-06-29 15:15:50 -0400

    Implementado subidor de archivos.

--- src/main/java/backend/BackendApplication.java
+++ src/main/java/backend/BackendApplication.java
@@ -1,10 +1,15 @@
 package backend;
+import backend.fileUploader.properties.FileStorageProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
+@EnableConfigurationProperties({
+		FileStorageProperties.class
+})
 public class BackendApplication {
 
 	public static void main(String[] args) {
