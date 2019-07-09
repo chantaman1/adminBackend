f187981e378b6ba746bb620bd8d8e44bb9935097
Francisco <francisco.diaz.s@usach.cl>
2019-07-09 14:14:34 -0400

    Agregcion test unitarios Arquitectura empresarial, arreglos menores test

--- src/test/java/backend/models/ArquitecturaEmpresarialTest.java
+++ src/test/java/backend/models/ArquitecturaEmpresarialTest.java
@@ -1,28 +1,33 @@
 package backend.models;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 public class ArquitecturaEmpresarialTest {
 
+    ArquitecturaEmpresarial empresarial;
+
+    @Before
+    public void testing(){
+        empresarial =  new ArquitecturaEmpresarial();
+        empresarial.set_id("123");
+        empresarial.setTitulo("Titulo Test");
+        empresarial.setDescripcion("Descripcion Test");
+    }
+
     @Test
     public void getIdTest(){
-        ArquitecturaEmpresarial empresarial =  new ArquitecturaEmpresarial();
-        empresarial.set_id("123");
         Assert.assertEquals("123", empresarial.get_id());
     }
 
     @Test
     public void getTituloTest(){
-        ArquitecturaEmpresarial empresarial = new ArquitecturaEmpresarial();
-        empresarial.setTitulo("Titulo Test");
         Assert.assertEquals("Titulo Test", empresarial.getTitulo());
     }
 
     @Test
     public void getDescripcionTest(){
-        ArquitecturaEmpresarial empresarial = new ArquitecturaEmpresarial();
-        empresarial.setDescripcion("Descripcion Test");
         Assert.assertEquals("Descripcion Test", empresarial.getDescripcion());
     }
 }
